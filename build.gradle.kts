dacdd4efc6469165a22eeecc5568bcc4b7b3d317
Severn Everett <severn.everett@gmail.com>
2023-04-19 08:41:25 +0200

    Upgrade Android version to 8.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     val kotlinVersion = "1.8.20"
-    val androidVersion = "7.4.2"
+    val androidVersion = "8.0.0"
     id("com.android.application") version androidVersion apply false
     id("com.android.library") version androidVersion apply false
     kotlin("android") version kotlinVersion apply false
