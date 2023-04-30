60b9b1f58b5b87b7808ddcc6b967edd0552e9601
Severn Everett <severn.everett@gmail.com>
2023-04-30 13:20:51 +0200

    Added example of third-party serde operations

--- app/src/main/java/com/severett/androidxdemo/model/Foo.kt
+++ app/src/main/java/com/severett/androidxdemo/model/Foo.kt
@@ -4,3 +4,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Foo(val fizz: String, val bazz: List<String>, val count: UInt)
+
+data class ThirdPartyFoo(val fizz: String, val bazz: List<String>, val count: UInt)
--- app/src/main/java/com/severett/androidxdemo/ui/serialization/SerializationFragment.kt
+++ app/src/main/java/com/severett/androidxdemo/ui/serialization/SerializationFragment.kt
@@ -9,6 +9,8 @@ import androidx.lifecycle.ViewModelProvider
 import com.severett.androidxdemo.R
 import com.severett.androidxdemo.databinding.FragmentSerializationBinding
 import com.severett.androidxdemo.model.Foo
+import com.severett.androidxdemo.model.ThirdPartyFoo
+import com.severett.androidxdemo.serde.ThirdPartyFooSerializer
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -46,22 +48,26 @@ class SerializationFragment : Fragment() {
             viewLifecycleOwner,
             deserializedFooTextView::setText
         )
+        binding.radioTypeNormal.isChecked = true
+        binding.radioTypeThirdParty.isChecked = false
         binding.runSerdeButton.setOnClickListener {
-            val foo = Foo(
-                fizz = binding.editTextFizz.text.toString(),
-                bazz = binding.editTextBazz.text.toString().split(","),
-                count = binding.editTextCount.text.toString().toUInt()
-            )
+            val fizz = binding.editTextFizz.text.toString()
+            val bazz = binding.editTextBazz.text.toString().split(",")
+            val count = binding.editTextCount.text.toString().toUInt()
+            val checkedType = binding.serializationTypeRadioGroup.checkedRadioButtonId
+            val (encodedStr, decodedStr) = if (checkedType == binding.radioTypeNormal.id) {
+                serdeFoo(fizz, bazz, count)
+            } else {
+                serdeThirdPartyFoo(fizz, bazz, count)
+            }
             binding.serializedFooLabel.text = resources.getString(
                 R.string.label_serialization_serialized
             )
-            val encodedFoo = Json.encodeToString(foo)
-            val decodedFoo: Foo = Json.decodeFromString(encodedFoo)
-            binding.serializedFooDisplay.text = encodedFoo
+            binding.serializedFooDisplay.text = encodedStr
             binding.deserializedFooLabel.text = resources.getString(
                 R.string.label_serialization_deserialized
             )
-            binding.deserializedFooDisplay.text = decodedFoo.toString()
+            binding.deserializedFooDisplay.text = decodedStr
         }
         return root
     }
@@ -69,5 +75,23 @@ class SerializationFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun serdeFoo(fizz: String, bazz: List<String>, count: UInt): Pair<String, String> {
+        val foo = Foo(fizz, bazz, count)
+        val encodedFoo = Json.encodeToString(foo)
+        val decodedFoo: Foo = Json.decodeFromString(encodedFoo)
+        return encodedFoo to decodedFoo.toString()
+    }
+
+    private fun serdeThirdPartyFoo(
+        fizz: String,
+        bazz: List<String>,
+        count: UInt
+    ): Pair<String, String> {
+        val foo = ThirdPartyFoo(fizz, bazz, count)
+        val encodedFoo = Json.encodeToString(ThirdPartyFooSerializer, foo)
+        val decodedFoo = Json.decodeFromString(ThirdPartyFooSerializer, encodedFoo)
+        return encodedFoo to decodedFoo.toString()
     }
 }
