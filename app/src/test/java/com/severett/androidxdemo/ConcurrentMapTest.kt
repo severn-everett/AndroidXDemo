56473c15e5f9d0fca9b376a9db29a79abc45c186
Severn Everett <severn.everett@gmail.com>
2023-05-03 16:05:09 +0200

    Added missing Lincheck demo code

--- app/src/test/java/com/severett/androidxdemo/ConcurrentMapTest.kt
+++ app/src/test/java/com/severett/androidxdemo/ConcurrentMapTest.kt
@@ -3,17 +3,19 @@ package com.severett.androidxdemo
 import org.jetbrains.kotlinx.lincheck.annotations.Operation
 import org.jetbrains.kotlinx.lincheck.annotations.Param
 import org.jetbrains.kotlinx.lincheck.check
+import org.jetbrains.kotlinx.lincheck.paramgen.IntGen
 import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingOptions
 import org.jetbrains.kotlinx.lincheck.strategy.stress.StressOptions
 import org.junit.jupiter.api.Test
 import java.util.concurrent.ConcurrentHashMap
 
 @Param(name = "key", gen = AnEnumGen::class, conf = "")
+@Param(name = "value", gen = IntGen::class, conf = "1:100")
 class ConcurrentMapTest {
     private val map = ConcurrentHashMap<AnEnum, List<Int>>()
 
     @Operation
-    fun add(@Param(name = "key") key: AnEnum, value: Int) {
+    fun add(@Param(name = "key") key: AnEnum, @Param(name = "value") value: Int) {
         val list = map[key]
         if (list == null) {
             map[key] = listOf(value)
