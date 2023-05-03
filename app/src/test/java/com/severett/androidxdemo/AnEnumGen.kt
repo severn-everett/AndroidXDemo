d210e0fee9c16cd2862405412132b32034bcd5ac
Severn Everett <severn.everett@gmail.com>
2023-05-03 08:55:52 +0200

    Fixing constructor of AnEnumGen to conform to implicit contract

--- app/src/test/java/com/severett/androidxdemo/AnEnumGen.kt
+++ app/src/test/java/com/severett/androidxdemo/AnEnumGen.kt
@@ -1,9 +1,10 @@
 package com.severett.androidxdemo
 
+import org.jetbrains.kotlinx.lincheck.RandomProvider
 import org.jetbrains.kotlinx.lincheck.paramgen.ParameterGenerator
 import kotlin.random.Random
 
-class AnEnumGen(configuration: String) : ParameterGenerator<AnEnum> {
+class AnEnumGen(randomProvider: RandomProvider, configuration: String) : ParameterGenerator<AnEnum> {
     override fun generate(): AnEnum {
         val values = AnEnum.values()
         return AnEnum.values()[Random.nextInt(0, values.size)]
